235e59e5bafa6422fe46466c7405528d16a74d67
tables <tables@noreply.codeberg.org>
2025-04-02 08:40:58 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,55 +1,29 @@
 let ts0 = +(new Date())
-let regexPath = new RegExp(/^\/[-a-z0-9]+$/)
-let regexName = new RegExp(/^[-A-Za-z0-9\s"']$/)
 
 $(document).ready(function(){
-  $('#noaava').DataTable({
+  $('#erica').DataTable({
     initComplete:()=>{
       let ts1 = +(new Date())
       console.log(`loaded in ${(ts1-ts0)/1000}s`)
     },
-    ajax: 'data/noaa-voices.json',
+    ajax: '/data/fulltext-eric-records-lite.json',
     order: {
-      name:'date',
+      name:'publicationdateyear',
       dir:'desc'
     },
     columns: [
       
-      {data:'title', name:'title', title:'interviewee', render:data=>{
-        return `<a href="https://web.archive.org/web/2/https://voices.nmfs.noaa.gov${data.href}" target="_blank">${data.text}</a>`
+      {data:'id', name:'id', title:'id', render:data=>{
+        if(data.match(/^E[DJ][0-9]{6,7}$/)) {
+          return `<a href="https://web.archive.org/web/2/https://files.eric.ed.gov/fulltext/${data}.pdf" target="_blank">${data}</a>`
+        } else {
+          return data
+        }
       }},
+      {data:'title', name:'title', title:'title'},
+      {data:'author[</br></br>]', name:'author', title:'author'},
+      {data:'publicationdateyear', name:'publicationdateyear', title:'year'}
       
-      {data:'interviewers', name:'interviewers', title:'interviewer', render:data=>{
-        return data.map(o=>`<a href="https://web.archive.org/web/2/https://voices.nmfs.noaa.gov${o.href}" target="_blank">${o.text}</a>`
-        ).join('<br/>')
-      }},
-      
-      {data:'date', name:'date', title:'date of interview', render:data=>{
-        if(data.text.trim().length) {
-          let dates = data.text.trim().split(',').map(date=>date.trim())
-          let datesISO = dates.map(date=>{
-            let [mo,da,yr] = date.split('-')
-            return [yr,mo,da].join('-')
-          })
-          return datesISO.join('<br/>')
-        } else return ''
-      }},
-
-      {data:'affiliations', name:'affiliations', title:'contributing organisation', render:data=>{
-        return data.map(o=>`<a href="https://web.archive.org/web/2/https://voices.nmfs.noaa.gov${o.href}" target="_blank">${o.text}</a>`
-        ).join('<br/>')
-      }},
-      
-      {data:'location', name:'location', title:'location of interview', render:data=>{
-        return `<a href="https://web.archive.org/web/2/https://voices.nmfs.noaa.gov${data.href}" target="_blank">${data.text}</a>`
-      }},
-
-      {data:'description.text', name:'description', title:'description'},
-      
-      {data:'collection', name:'collection', title:'collection name', render:data=>{
-        return `<a href="https://web.archive.org/web/2/https://voices.nmfs.noaa.gov${data.href}" target="_blank">${data.text}</a>`
-      }},
-
     ]
   });
 });
